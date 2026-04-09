21b6ee3fcef03644d8019348aee4cbca4d479a94
Ryan-Merchant <52jjd2fhs8@privaterelay.appleid.com>
2026-04-08 21:04:37 -0500

    Reworked exponentiation
    Adjusted exponentiation method to be fast instead of linear

--- src/FastExponentiation.java
+++ src/FastExponentiation.java
@@ -5,14 +5,19 @@ public class FastExponentiation {
 	public static void main(String[] args) 
 	{
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter a base value: ");
-		int x = scan.nextInt();
+		long x = 2;
+		System.out.println("The base vaue is: " + x);
 		System.out.print("Enter the value of the exponenet: ");
-		int n = scan.nextInt();
+		long n = scan.nextLong();
+		System.out.println("Enter the number of test runs: ");
+		long numRuns = scan.nextLong();
+		scan.close();
 		
+		long recursion = 0;
+		long iterative = 0;
 		try
 		{
-			averageRecursion(x, n, 10);
+			recursion = averageRecursion(x, n, numRuns);
 		}
 		catch (StackOverflowError e)
 		{
@@ -21,32 +26,51 @@ public class FastExponentiation {
 		
 		try
 		{
-			averageIterative(x, n, 10);
+			iterative = averageIterative(x, n, numRuns);
 		}
 		catch (StackOverflowError e)
 		{
 			System.out.println("Iterative StackOverflowError");
 		}
+		System.out.println("\n\n");
+		System.out.println("Recursive average runtime: " + recursion);
+		System.out.println("Iterative average runtime: " + iterative);
+
 	}
 	
-	public static int recursiveExponentiation(int x, int n) //a ^ n
+	public static long recursiveExponentiation(long x, long n) //a ^ n
 	{
 		if (n == 0)
-			return 1;
-		return recursiveExponentiation(x, n-1) * x;
-	}
-	
-	public static int iterativeExponentiation(int x, int n)
-	{
-		int num = x;
-		for(int i = 1; i < n; i++)
 		{
-			num *= x;
+			return 1;
 		}
-		return num;
+		if (n % 2 == 0)
+		{
+			long half = recursiveExponentiation(x, n/2);
+			return half * half;
+		}
+		else
+		{
+			return x * recursiveExponentiation(x, n-1);
+		}
 	}
 	
-	public static void averageRecursion(int x, int n, int numRuns)
+	public static long iterativeExponentiation(long x, long n)
+	{
+		long result = 1;
+		while (n > 0)
+		{
+			if (n % 2 != 0)
+			{
+				result *= x;
+			}
+			x *= x;
+			n /= 2;
+		}
+		return result;
+	}
+	
+	public static long averageRecursion(long x, long n, long numRuns)
 	{
 		long totalTime = 0;
 		for(int i = 1; i <= numRuns; i++)
@@ -59,18 +83,21 @@ public class FastExponentiation {
 		}
 		System.out.println();
 		System.out.println("Recursion average runtime: " + totalTime/numRuns);
+		System.out.println();
+		return (totalTime/numRuns);
 	}
-	public static void averageIterative(int x, int n, int numRuns)
+	public static long averageIterative(long x, long n, long numRuns)
 	{
 		long totalTime = 0;
 		for(int i = 1; i <= numRuns; i++)
 		{
 			long start = System.nanoTime();
-			recursiveExponentiation(x, n);
+			iterativeExponentiation(x, n);
 			long runTime = System.nanoTime() - start;
 			System.out.println("Iterative run #" + i + ": " + runTime);
 			totalTime += runTime;
 		}
 		System.out.println("Iterative average runtime: " + totalTime/numRuns);
+		return (totalTime/numRuns);
 	}
 }
